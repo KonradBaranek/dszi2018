cc4ee4218cfc02e588b803c16a30a1ea079ded0b
Aleksander <aleksander.piotrowski@recoded.co>
2018-11-05 19:57:37 +0100

    kick off

---
+++ README.md
@@ -0,0 +1,1 @@
+[Adres developerski](http://127.0.0.1:8080/)
---
+++ src/index.js
@@ -0,0 +1,11 @@
+// eslint powinien rzucać waringiem
+console.log('TU ROBIMY CAŁĄ LOGIKĘ');
+
+// eslint powinien rzucać błędem.
+let a = 5;
+let b = 10;
+let c = 111;
+
+const array = [10, 20, 30];
+
+console.log(a + b + c, [...array], ...array);
